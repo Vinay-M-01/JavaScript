19d8d1f2d5fb676b2d47da8b7f386a0a2927e20e
Vinay-M-01 <98087682+Vinay-M-01@users.noreply.github.com>
2022-06-08 12:30:36 +0530

    serialized object creation

--- Forms/script.js
+++ Forms/script.js
@@ -3,8 +3,22 @@ formName.addEventListener('submit',sendData)
 
 function sendData(e)
 {
-    localStorage.setItem('The Name is',document.getElementById("fname").value)
-    localStorage.setItem('The Email is',document.getElementById("email").value)
-    localStorage.setItem('The Phone Number is',document.getElementById("phone").value)
-    localStorage.setItem('The Meeting time is',document.getElementById("birthdaytime").value)
+    e.preventDefault()
+    let newObj = {
+        "name" : document.getElementById("fname").value,
+        "Email" : document.getElementById("email").value,
+        "phone": document.getElementById("phone").value,
+        "meeting time": document.getElementById("birthdaytime").value
+    }
+    
+    let serializedObj = JSON.stringify(newObj);
+    console.log(serializedObj);
+
+    localStorage.setItem("newdata", serializedObj)
+
+//     localStorage.setItem('The Name is',document.getElementById("fname").value)
+//     localStorage.setItem('The Email is',document.getElementById("email").value)
+//     localStorage.setItem('The Phone Number is',document.getElementById("phone").value)
+//     localStorage.setItem('The Meeting time is',document.getElementById("birthdaytime").value)
+// 
 }
